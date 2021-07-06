7bf42c25e6b7d4c37fc9561dcd29427579276e5c
kv3delacruz <kv3delacruz@gmail.com>
2021-07-06 17:28:48 -0600

    adding vuex modules and pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,31 +1,37 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
+    <h1>Events Listing for {{user.user.name}}</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list',query: { page: page-1} }" rel="prev">
+      Prev Page</router-link>
+    </template>
+    |
+    <template v-if=" totalEvents > (page * 3)">
+      <router-link :to="{ name: 'event-list',query: { page: page+1} }" rel="next">
+        Next Page</router-link>
+    </template>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
-
+import { mapState } from 'vuex'
 export default {
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log('There was an error:', error.response)
-      })
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
+  },
+  computed: {
+    ...mapState(['events', 'totalEvents', 'user']),
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    }
   }
 }
 </script>
